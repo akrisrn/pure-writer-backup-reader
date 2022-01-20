6066d091a7d76be65ea91f51038a162509dfd2be
AkrISrn <shairkarine@gmail.com>
2022-01-20 09:07:25 +0800

    build: init

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const nodeExternals = require('webpack-node-externals');
+
+const context = path.join(__dirname, 'src');
+
+module.exports = {
+  node: false,
+  target: 'node',
+  externals: [
+    nodeExternals(),
+  ],
+  context,
+  entry: {
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.ts'],
+    alias: {
+      '@': context,
+    },
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].js',
+  },
+};
