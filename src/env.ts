65854bdf9996d4d9d5de62ce2f5a720c9f81b00a
AkrISrn <shairkarine@gmail.com>
2022-01-20 19:32:20 +0800

    feat(env): init
    - BACKUPS_PATH
    - TARGET_PATH
    - WATCH
    - INTERVAL
    - WEB_DAV
    - WEB_DAV_URL
    - WEB_DAV_USERNAME
    - WEB_DAV_PASSWORD

---
+++ src/env.ts
@@ -0,0 +1,20 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+function parseNum(value: string | undefined, defaultValue: number) {
+  if (!value) {
+    return defaultValue;
+  }
+  const num = parseInt(value);
+  return isNaN(num) ? defaultValue : num;
+}
+
+export const BACKUPS_PATH = process.env.BACKUPS_PATH || '/PureWriter/Backups';
+export const TARGET_PATH = process.env.TARGET_PATH || 'PureWriter';
+export const WATCH = !!process.env.WATCH;
+export const INTERVAL = parseNum(process.env.INTERVAL, 1000 * 60 * 10);
+export const WEB_DAV = !!process.env.WEB_DAV;
+export const WEB_DAV_URL = process.env.WEB_DAV_URL || '';
+export const WEB_DAV_USERNAME = process.env.WEB_DAV_USERNAME || '';
+export const WEB_DAV_PASSWORD = process.env.WEB_DAV_PASSWORD || '';
