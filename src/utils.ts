94f789e0c758653b3ab7f32fa722a62498959425
AkrISrn <shairkarine@gmail.com>
2022-01-21 11:10:25 +0800

    feat(utils): add yieldFilePath and watchDir

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,32 @@
+import fs from 'fs';
+import watch from 'node-watch';
+import path from 'path';
+
 export function log(message?: any, ...optionalParams: any[]) {
   console.log(`[${ new Date().toJSON() }]`, message, ...optionalParams);
 }
 
 export function error(message?: any, ...optionalParams: any[]) {
   log('[error]', message, ...optionalParams);
+}
+
+export function* yieldFilePath(dirPath: string, match: RegExp) {
+  for (const dirent of fs.readdirSync(dirPath, {
+    withFileTypes: true,
+  })) {
+    if (!dirent.isDirectory() && match.test(dirent.name)) {
+      yield path.join(dirPath, dirent.name);
+    }
+  }
+}
+
+type EventType = 'update' | 'remove';
+type Callback = (eventType?: EventType, filePath?: string) => any;
+
+export function watchDir(dirPath: string, match: RegExp, callback: Callback) {
+  return watch(dirPath, {
+    filter(file) {
+      return match.test(file);
+    },
+  }, callback);
 }
