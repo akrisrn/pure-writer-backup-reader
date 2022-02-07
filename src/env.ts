c2851c51d249b7e8b74561ada54c9df9eedf41fa
AkrISrn <shairkarine@gmail.com>
2022-02-07 10:43:32 +0800

    feat(env): add isTrue

--- src/env.ts
+++ src/env.ts
@@ -2,6 +2,10 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+function isTrue(value: string | undefined) {
+  return !!(value && ['true', 'enable'].includes(value.toLowerCase()));
+}
+
 function parseNum(value: string | undefined, defaultValue: number) {
   if (!value) {
     return defaultValue;
@@ -12,10 +16,10 @@ function parseNum(value: string | undefined, defaultValue: number) {
 
 export const BACKUPS_PATH = process.env.BACKUPS_PATH || '/PureWriter/Backups';
 export const TARGET_PATH = process.env.TARGET_PATH || 'PureWriter';
-export const WATCH = !!process.env.WATCH;
+export const WATCH = isTrue(process.env.WATCH);
 export const INTERVAL = parseNum(process.env.INTERVAL, 1000 * 60 * 10);
-export const ENABLE_GIT = !!process.env.ENABLE_GIT;
-export const WEB_DAV = !!process.env.WEB_DAV;
+export const ENABLE_GIT = isTrue(process.env.ENABLE_GIT);
+export const WEB_DAV = isTrue(process.env.WEB_DAV);
 export const WEB_DAV_URL = process.env.WEB_DAV_URL || '';
 export const WEB_DAV_USERNAME = process.env.WEB_DAV_USERNAME || '';
 export const WEB_DAV_PASSWORD = process.env.WEB_DAV_PASSWORD || '';
